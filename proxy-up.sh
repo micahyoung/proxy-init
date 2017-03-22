d59a6881f0ce7233dff0830dd324553074a35bc5
Micah Young <micah@young.io>
2017-03-22 19:52:31 -0400

    Bump disk size to 10

--- proxy-up.sh
+++ proxy-up.sh
@@ -4,7 +4,7 @@ dom_name=proxy-0
 if virsh list --all | grep $dom_name; then
   virsh start $dom_name
 else
-  uvt-kvm create --cpu=1 --memory=1024 --disk=5 --run-script-once=proxy-vm.sh $dom_name release=xenial arch=amd64
+  uvt-kvm create --cpu=1 --memory=1024 --disk=10 --run-script-once=proxy-vm.sh $dom_name release=xenial arch=amd64
 fi
 
 while ! uvt-kvm ip $dom_name; do sleep 1; done
