a276af7ecaa6f0fef78cf3d4e43c9833333bb17c
Micah Young <myoung@pivotal.io>
2017-04-12 23:35:18 -0400

    Rewrite up script to compress
    - Refactor sizes to variables

--- proxy-up.sh
+++ proxy-up.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 dom_name=proxy-0
+script=proxy-vm.sh
+memory_mb=1024
+disk_gb=10
 if virsh list --all | grep $dom_name; then
   virsh start $dom_name
 else
-  uvt-kvm create --cpu=1 --memory=1024 --disk=10 --run-script-once=proxy-vm.sh $dom_name release=xenial arch=amd64
+
+cat > uvt-wrapper.sh <<EOF
+cat << EOF2 | base64 -d  | tar xJ > $script
+$(tar cJ $script | base64 -w0)
+EOF2
+./$script $(echo $*)
+EOF
+
+  uvt-kvm create --cpu=1 --memory=$memory_mb --disk=$disk_gb --run-script-once=uvt-wrapper.sh $dom_name release=xenial arch=amd64
 fi
 
 while ! uvt-kvm ip $dom_name; do sleep 1; done
